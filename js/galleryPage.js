34eefe7dc553865eb32b3b3a516df3c2e82356e5
Gary Zhang <gary@moocads.com>
2021-03-26 18:58:07 -0400

    update blog page

--- js/galleryPage.js
+++ js/galleryPage.js
@@ -4,39 +4,36 @@ console.log(galleryPageUrl)
 // Projects Page Gallery Display
 $.getJSON(galleryPageUrl, function (gallery) {
   console.log(gallery)
-
   document.getElementById('masonry').innerHTML = `
   ${gallery
     .map(function (projects) {
       return `
       <li
-  data-category="${projects.categories.cate}"
-  class="card-container col-lg-3 col-md-4 col-sm-6 p-lr0 doorwindow wow fadeIn"
-  data-wow-duration="2s"
-  data-wow-delay="0.2s"
-><div
-class="dlab-media dlab-img-overlay1 dlab-img-effect portbox1"
->
-<img src="${projects.FeatureImage.url}" alt="" />
-<div class="overlay-bx">
-  <div class="portinner">
-    <h3 class="port-title">
-      <a href="project-detail-1.html"
-        >${projects.ProjectName}</a
-      >
-    </h3>
-    <a
-      href="project-detail-1.html"
-      class="btn btn-primary m-t15"
-      >查看项目</a
-    >
-  </div>
-</div>
-</div>
-</li>
+        data-category="${projects.categories.cate}"
+        class="card-container col-lg-3 col-md-4 col-sm-6 p-lr0 doorwindow wow fadeIn"
+        data-wow-duration="2s"
+        data-wow-delay="0.2s">
+        <div class="dlab-media dlab-img-overlay1 dlab-img-effect portbox1">
+          <img src="${projects.FeatureImage.url}" alt="" class="index-gallery-imgs"/>
+          <div class="overlay-bx">
+            <div class="portinner">
+              <h3 class="port-title">
+                <a href="project-detail-1.html"
+                  >${projects.ProjectName}</a
+                >
+              </h3>
+              <a
+                href="project-detail-1.html"
+                class="btn btn-primary m-t15"
+                >查看项目</a
+              >
+            </div>
+          </div>
+        </div>
+      </li>
       `
     })
-    .join('')}
+    .join(' ')}
   `
 })
 // function galleryTemplate()
