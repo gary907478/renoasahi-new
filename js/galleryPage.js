c0e1596e488d1618a1c977dd56fc998af891f366
Gary Zhang <gary@moocads.com>
2021-05-10 16:19:48 -0400

    gallery page add pagination; working on google translation

--- js/galleryPage.js
+++ js/galleryPage.js
@@ -26,13 +26,12 @@ $.ajax({
   url: 'https://renoasahi-cms.herokuapp.com/projects?_sort=created_at:DESC',
   type: 'GET',
   success: function (data) {
-    console.log(data)
     var indexGalleryContainer = $('#masonry')
     data.forEach((item) => {
       indexGalleryContainer.append(`
       <li 
       data-category="${item.categories.cate}"
-      class="index-gallery-item2 filtermark"
+      class="index-gallery-item2 filtermark gallery-item"
             data-wow-delay="0.2s" data-wow-duration="2s">
             <a href="portfolio-detail.html?id=${item._id}" 
               class="index-gallery-item2 img-zoom-wrap ig-filter ig-doorwindow">
@@ -48,10 +47,28 @@ $.ajax({
           </li>
       `)
     })
-    openTab('All')
+
+    var gitems = $('.index-gallery-grid2 .gallery-item')
+    var perPage = 8
+    var numItems = gitems.length
+    console.log(numItems)
+    gitems.slice(perPage).hide()
+    $('#pagination-container').pagination({
+      items: numItems,
+      itemsOnPage: perPage,
+      prevText: '&laquo;',
+      nextText: '&raquo;',
+      onPageClick: function (pageNumber) {
+        var showFrom = perPage * (pageNumber - 1)
+        var showTo = showFrom + perPage
+        gitems.hide().slice(showFrom, showTo).show()
+      },
+    })
+    // openTab('All')
   },
 })
 function openTab(category) {
+  var cateItems = $('#masonry .filtermark')
   $('#masonry .filtermark').each(function () {
     var el = $(this)
     if (el.attr('data-category') === category || category === 'All') {
@@ -60,4 +77,44 @@ function openTab(category) {
       el.hide()
     }
   })
+  var matched = cateItems.filter(function () {
+    return $(this).attr('data-category') === category || category === 'All'
+  })
+  var perPage = 8
+  var numItems = matched.length
+  matched.slice(perPage).hide()
+  $('#pagination-container').pagination({
+    items: numItems,
+    itemsOnPage: perPage,
+    prevText: '&laquo;',
+    nextText: '&raquo;',
+    onPageClick: function (pageNumber) {
+      var showFrom = perPage * (pageNumber - 1)
+      var showTo = showFrom + perPage
+      matched.hide().slice(showFrom, showTo).show()
+    },
+  })
+
+  // var perPage = 8
+  // var numItems = cateItems.length
+  // cateItems.slice(perPage).hide()
+  // $('#pagination-container').pagination({
+  //   items: numItems,
+  //   itemsOnPage: perPage,
+  //   prevText: '&laquo;',
+  //   nextText: '&raquo;',
+  //   onPageClick: function (pageNumber) {
+  //     var showFrom = perPage * (pageNumber - 1)
+  //     var showTo = showFrom + perPage
+  //     cateItems.hide().slice(showFrom, showTo).show()
+  //   },
+  // })
+  // $('#masonry .filtermark').each(function () {
+  //   var el = $(this)
+  //   if (el.attr('data-category') === category || category === 'All') {
+  //     el.show()
+  //   } else {
+  //     el.hide()
+  //   }
+  // })
 }
